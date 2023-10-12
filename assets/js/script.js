286924961dc473bbdf5697e0d572d1e6058cbaff
sasholilov <alex_xld@abv.bg>
2023-10-13 01:41:43 +0300

    Complete My Projects Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 const btnPlus = document.querySelector(".about-more-info");
 const cont = document.querySelector(".about-me-info");
+const projectOne = document.querySelector(".project-one");
+const btnView = document.querySelector(".button-view");
+const imgEl = document.querySelector(".project-one").querySelector("img");
+const prjTech = document
+  .querySelector(".project-technologies")
+  .querySelectorAll("span");
+
 btnPlus.addEventListener("click", function () {
   cont.classList.remove("hidden");
   if (btnPlus.classList.contains("close-info")) {
@@ -10,6 +17,40 @@ btnPlus.addEventListener("click", function () {
     btnPlus.classList.add("close-info");
     btnPlus.textContent = "-";
   }
+});
+
+projectOne.addEventListener("mouseout", function () {
+  prjTech.forEach((e) => {
+    e.style.display = "flex";
+  });
+  btnView.style.opacity = "0";
+  imgEl.style.opacity = "1";
+  if (imgEl.style.opacity === "1") {
+    btnView.style.opacity = "0";
+  }
+});
+
+projectOne.addEventListener("mouseover", function () {
+  prjTech.forEach((e) => {
+    e.style.display = "none";
+  });
+  if (imgEl.style.opacity === "1") {
+    btnView.style.opacity = "0";
+  }
+  setTimeout(function () {
+    btnView.style.opacity = "1";
+  }, 200);
+  projectOne.style.transition = "0.3s";
+  projectOne.position = "relative";
+  imgEl.style.opacity = "0.7";
+});
+
+btnView.addEventListener("mouseover", function () {
+  if (imgEl.style.opacity === "1") {
+    btnView.style.opacity = "0";
+  }
+  imgEl.style.opacity = "0.7";
+  btnView.style.opacity = "1";
 });
 
 function calcMyAge() {
