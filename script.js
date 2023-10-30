201b2a314df310064637e09b09746b321b5b1e72
sasholilov <alex_xld@abv.bg>
2023-10-31 00:31:57 +0200

    fix the horizontal scroll on mobile

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const btnPlus = document.querySelector(".about-more-info");
 const cont = document.querySelector(".about-me-info");
-const projectOne = document.querySelector(".project-one");
-const btnView = document.querySelector(".button-view");
-const imgEl = document.querySelector(".project-one").querySelector("img");
-const prjTech = document
+const projectOne = document.getElementById("project-one");
+const projectTwo = document.getElementById("project-two");
+const btnViewOne = document
+  .getElementById("project-one")
+  .querySelector(".button-view");
+const btnViewTwo = document
+  .getElementById("project-two")
+  .querySelector(".button-view");
+const imgElOne = document.getElementById("project-one").querySelector("img");
+const imgElTwo = document.getElementById("project-two").querySelector("img");
+const prjTechOne = document
+  .getElementById("project-one")
   .querySelector(".project-technologies")
   .querySelectorAll("span");
-
+const prjTechTwo = document
+  .getElementById("project-two")
+  .querySelector(".project-technologies")
+  .querySelectorAll("span");
 const firstName = document.querySelector('input[id="firstName"]');
 const lastName = document.querySelector('input[id="lastName"]');
 const email = document.querySelector('input[id="email"]');
 const subject = document.querySelector('input[id="subject"]');
 const textMsg = document.querySelector("textarea");
 const succesMsg = document.getElementById("succes-sent");
-
 const msgObject = {
   FirstName: firstName.value,
   LastName: lastName.value,
@@ -21,7 +31,6 @@ const msgObject = {
   Subject: subject.value,
   Message: textMsg.value,
 };
-
 document.getElementById("btnMsg").addEventListener("click", function () {
   // Проверете дали всички полета са попълнени
   if (
@@ -78,38 +87,76 @@ btnPlus.addEventListener("click", function () {
 });
 
 projectOne.addEventListener("mouseout", function () {
-  prjTech.forEach((e) => {
+  prjTechOne.forEach((e) => {
     e.style.display = "flex";
   });
-  btnView.style.opacity = "0";
-  imgEl.style.opacity = "1";
-  if (imgEl.style.opacity === "1") {
-    btnView.style.opacity = "0";
+  btnViewOne.style.opacity = "0";
+  imgElOne.style.opacity = "1";
+  if (imgElOne.style.opacity === "1") {
+    btnViewOne.style.opacity = "0";
   }
 });
 
 projectOne.addEventListener("mouseover", function () {
-  prjTech.forEach((e) => {
+  prjTechOne.forEach((e) => {
     e.style.display = "none";
   });
-  if (imgEl.style.opacity === "1") {
-    btnView.style.opacity = "0";
+  if (imgElOne.style.opacity === "1") {
+    btnViewOne.style.opacity = "0";
   }
   setTimeout(function () {
-    btnView.style.opacity = "1";
+    btnViewOne.style.opacity = "1";
   }, 200);
   projectOne.style.transition = "0.3s";
   projectOne.position = "relative";
-  imgEl.style.opacity = "0.7";
+  imgElOne.style.opacity = "0.7";
 });
 
-btnView.addEventListener("mouseover", function () {
-  if (imgEl.style.opacity === "1") {
-    btnView.style.opacity = "0";
+btnViewOne.addEventListener("mouseover", function () {
+  if (imgElOne.style.opacity === "1") {
+    btnViewOne.style.opacity = "0";
   }
-  imgEl.style.opacity = "0.7";
-  btnView.style.opacity = "1";
+  imgElOne.style.opacity = "0.7";
+  btnViewOne.style.opacity = "1";
 });
+
+//------------------//
+
+projectTwo.addEventListener("mouseout", function () {
+  prjTechTwo.forEach((e) => {
+    e.style.display = "flex";
+  });
+  btnViewTwo.style.opacity = "0";
+  imgElTwo.style.opacity = "1";
+  if (imgElTwo.style.opacity === "1") {
+    btnViewTwo.style.opacity = "0";
+  }
+});
+
+projectTwo.addEventListener("mouseover", function () {
+  prjTechTwo.forEach((e) => {
+    e.style.display = "none";
+  });
+  if (imgElTwo.style.opacity === "1") {
+    btnViewTwo.style.opacity = "0";
+  }
+  setTimeout(function () {
+    btnViewTwo.style.opacity = "1";
+  }, 200);
+  projectTwo.style.transition = "0.3s";
+  projectTwo.position = "relative";
+  imgElTwo.style.opacity = "0.7";
+});
+
+btnViewTwo.addEventListener("mouseover", function () {
+  if (imgElTwo.style.opacity === "1") {
+    btnViewTwo.style.opacity = "0";
+  }
+  imgElTwo.style.opacity = "0.7";
+  btnViewTwo.style.opacity = "1";
+});
+
+//-----------------//
 
 function calcMyAge() {
   return new Date().getFullYear() - 1990;
